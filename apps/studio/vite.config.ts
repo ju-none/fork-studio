77430e6d3e9d88235eb1322c17e06ed12ff7fc97
ju-none <julien.leclercqprof@gmail.com>
2025-05-21 12:36:07 +0200

    fix

--- apps/studio/vite.config.ts
+++ apps/studio/vite.config.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/',
+  base: '/fork-studio/',
   plugins: [react()],
   build: {
     target: "esnext",
@@ -18,6 +18,6 @@ export default defineConfig({
     "process.env": {},
   },
   server: {
-    port: 5174,
+    port: 5173,
   }
 });
